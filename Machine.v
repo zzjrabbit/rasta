a304b5f2582e1c54dd5402e8cae3b677bbbcf2c7
zzjrabbit <239873059@qq.com>
2024-09-21 21:28:49 +0800

    better example

--- Machine.v
+++ Machine.v
@@ -1,4 +1,4 @@
 module Machine(input [63:0] a,input [63:0] b,output [63:0]  out);
-assign out = a + b + 1;
+assign out = a * b;
 endmodule
 
--- rasta-verilog/examples/test/main.rs
+++ rasta-verilog/examples/test/main.rs
@@ -1,21 +1,30 @@
-use std::fs::File;
-use std::io::Write;
+use std::{env::args, fs::File};
+use std::io::{Read, Write};
 
 use parser::parse;
 use rasta_verilog::{generate_verilog, TOP_MODULE};
 
 mod parser;
 
-const CODE: &'static str = "
-#[top]
-const Machine = fn (arg a: u64,arg b: u64) -> u64 {
-    return a+b;
-};
-";
-
 fn main() {
 
-    let code = generate_verilog(&parse(CODE.into(), "".into())).unwrap();
+    let mut args = args();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} <source_file>", args.next().unwrap());
+        std::process::exit(1);
+    }
+
+    let _exe = args.nth(0).unwrap();
+    let source_file_path = args.nth(0).unwrap();
+
+    let mut source_file = File::open(source_file_path.clone()).unwrap();
+    let mut source = String::new();
+    source_file.read_to_string(&mut source).unwrap();
+
+    let ast = parse(source, source_file_path);
+
+    let code = generate_verilog(&ast).unwrap();
 
     let top = TOP_MODULE.lock().unwrap().clone().unwrap();
 
